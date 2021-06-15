fc0e669bc10fb2654fde8b93644c9ab0d213f950
Denise PC <43567020+Denijar@users.noreply.github.com>
2021-06-16 11:05:33 +1200

    scripts edited to accomodate typescript. GitHub action updated

--- server/controllers/hello.controller.ts
+++ server/controllers/hello.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-const hello = (req: Request, res: Response, next: NextFunction) => {
+export const hello = (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
     body: "Hello from the typescript server!"
   });
